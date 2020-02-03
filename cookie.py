77b88057262c76461332c83728cea5430c407f38
Виктор Никель <v.nikell.28@gmail.com>
2020-02-03 13:02:23 +0300

    Removal

---
+++ cookie.py
@@ -0,0 +1,25 @@
+cookie={
+        dg5_flags=; 
+        dscnt=1; 
+        _2gis_webapi_user=464da36e-a8b8-46e8-9a7c-8375e2789edf; 
+        openBeta=1; 
+        _ga=GA1.2.1427680370.1580240917; 
+        _ym_uid=1580240917438934614; 
+        _ym_d=1580240917; 
+        __gads=ID=33c5cfcda78f38a0:T=1580469679:S=ALNI_MYZDwKRKcID0kIl6QEuJ3jlkH5KZA;
+        _2gis_webapi_user=d4dd3826-c732-446c-88cb-a55d9a05e503; 
+        lang=ru; __utma=64270060.1427680370.1580240917.1580241048.1580292832.3; 
+        __utmz=64270060.1580241048.2.2.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); 
+        dg5_pos=37.646892%3B55.724998%3B9.85; 
+        ipp_uid=1580308523323%2fX0tBEIk6S4c2RsT7%2fDjVd9mZp5y5KWeYycjcH7w%3d%3d; 
+        ipp_uid1=1580308523323; 
+        ipp_uid2=X0tBEIk6S4c2RsT7%2fDjVd9mZp5y5KWeYycjcH7w%3d%3d; 
+        ipp_uid_tst=; ipp_static_key=; _gid=GA1.2.1186107280.1580463129; 
+        _2gis_webapi_session=9be54a41-2a07-4c4e-94a2-7fa20ed63409; 
+        _2gis_webapi_session=a59cdcbe-c514-4bec-bdf9-5cc4940e80d8; 
+        __utmc=64270060; 
+        ipp_key=v1580558646179%2Fv3394bd400b5e53a13cfc65163aeca6afa04ab3%2FJcfTeaV0fdykup%2fwCTq%2bnA%3d%3d; 
+        _ym_isad=2; 
+        _ym_visorc_837864=b; 
+        
+    }
